1fc0f94b3ccb7ddbb422ce43a52fc67c81648628
raihan <ryihan55@gmail.com>
2021-05-20 06:04:38 +0600

    Update Invoice.php

--- includes/payment/methods/vendor/bitpay/php-client/src/Bitpay/Invoice.php
+++ includes/payment/methods/vendor/bitpay/php-client/src/Bitpay/Invoice.php
@@ -1,3 +1,17 @@
+<?php
+/**
+ * @license Copyright 2011-2014 BitPay Inc., MIT License
+ * see https://github.com/bitpay/php-bitpay-client/blob/master/LICENSE
+ */
+
+namespace Bitpay;
+
+/**
+ *
+ * @package Bitpay
+ */
+class Invoice implements InvoiceInterface
+{
     public function setPaymentTotals($paymentTotals)
     {
         if (!empty($paymentTotals)) {
