8c59c24d77608b490f48eab0f7603b54582b0c2c
raihan <ryihan55@gmail.com>
2021-05-20 06:09:44 +0600

    Create GetTokens.php

---
+++ includes/payment/methods/vendor/bitpay/php-client/examples/GetTokens.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Copyright (c) 2014-2015 BitPay
+ */
+
+/**
+ * WARNING - This example will NOT work until you have generated your public
+ * and private keys. Please see the example documentation on generating your
+ * keys and also see the documentation on how to save those keys.
+ *
+ * Also please be aware that you CANNOT create an invoice until you have paired
+ * the keys and received a token back. The token is usesd with the request.
+ */
+
+require __DIR__ . '/../vendor/autoload.php';
+
+
+/**
+ * Create a new client. You can see the example of how to configure this using
+ * a yml file as well.
+ */
