774760dd2da90a638d4cabc339b26444a488c406
raihan <ryihan55@gmail.com>
2021-05-20 06:14:21 +0600

    Update popup.php

--- admin/default/request/popup.php
+++ admin/default/request/popup.php
@@ -1,3 +1,13 @@
 <?php 
 include_once "../../../includes/inc.php";    
 $statusValue = array('0','1');
+if(isset($_POST['f']) && $logedIn == '1'){
+  $type = mysqli_real_escape_string($db, $_POST['f']);
+  /*Delete Post Call AlertBox*/
+  if($type == 'ddelPost'){
+    if(isset($_POST['id'])){
+      $postID = mysqli_real_escape_string($db, $_POST['id']);
+      $alertType = $type;
+      include("../sources/popup/deletePost.php");
+    }
+  }
